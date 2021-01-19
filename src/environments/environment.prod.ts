1ea249c9ac29583862ab10e687e13c670ea0d73c
alvarodachez <sanchez99esp@gmail.com>
2021-01-19 19:53:46 +0100

    añadida environmentprod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAp5LiWpDexp7IKwy0hQ9i6HOud1MMTkfg",
+    authDomain: "firechat-8a5e3.firebaseapp.com",
+    projectId: "firechat-8a5e3",
+    storageBucket: "firechat-8a5e3.appspot.com",
+    messagingSenderId: "298593796766",
+    appId: "1:298593796766:web:3b9452763fdfea3975f18f",
+    measurementId: "G-KBP3EL6NHN"
+  }
 };
